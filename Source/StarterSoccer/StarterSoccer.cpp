d4354fe414a1c4e9f94117bb1c52cbdea183de76
Jmack <josephmmackey@gmail.com>
2024-11-24 02:10:33 -0700

    Starter soccer!

---
+++ Source/StarterSoccer/StarterSoccer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StarterSoccer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StarterSoccer, "StarterSoccer" );
