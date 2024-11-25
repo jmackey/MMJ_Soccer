3deed99c209db4557c1d8679ce74932830ecbdaa
Jmack <josephmmackey@gmail.com>
2024-11-25 06:34:57 -0700

    New BP project

--- Source/StarterSoccer/StarterSoccer.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "StarterSoccer.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StarterSoccer, "StarterSoccer" );
